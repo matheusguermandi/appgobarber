e8ad7fb95dfb8deef7c9ed9364820c25b8737e86
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-20 22:21:36 -0300

    Atualizando o index.tsx das rotas para inicializar em SignUP

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,8 @@ const AuthRouter: React.FC = () => (
       headerShown: false,
       cardStyle: { backgroundColor: '#312e38' },
     }}
+    /* Utilizada para definir a rota inicial da aplicação */
+    initialRouteName="SignUp"
   >
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
