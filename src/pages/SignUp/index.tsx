57356e7dde6b614f63b56b25b34d1805f9b5f065
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-20 22:44:51 -0300

    Construindo página a SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
 
-const SignUp: React.FC = () => {
-  return <Container />;
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <>
+      {/* Utilizado para subir o conteudo da tela do app quando o teclado é acionado */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        {/*
+          Utilizado para gerar um scroll quando a tela é pequena
+          keyboardShouldPersistTaps="handled" => Configuração para quando o teclado estiver aberto
+          e relar fora o mesmo seja fechado
+        */}
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu cadastro</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => {}}>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => {}}>
+        <Icon name="arrow-left" size={20} color="#f4ede8" />
+        <BackToSignInText>Voltar para Logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
-export default SignUp;
+export default SignIn;
