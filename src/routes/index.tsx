d3d0f4663cc9b98bf77d9904750e41f9ddd3cf78
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-24 15:45:09 -0300

    Colocando o loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
 import AuthRouter from './auth.routes';
 import AppRouter from './app.routes';
 
 import { useAuth } from '../hooks/auth';
 
 const Routes: React.FC = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#ff9000" />
+      </View>
+    );
+  }
 
   return user ? <AppRouter /> : <AuthRouter />;
 };
