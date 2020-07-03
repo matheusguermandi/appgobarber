87c42c07db2195f6455938c082f1fce748f37e82
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-07-02 23:00:19 -0300

    Atualizando app routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 
@@ -16,7 +19,11 @@ const AppRouter: React.FC = () => (
       initialRouteName="SignUp"
     */
   >
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
     <App.Screen name="Dashboard" component={Dashboard} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
