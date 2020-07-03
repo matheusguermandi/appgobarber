8c6c208d5d701095ffd3fc524dbfb1ac868c6adc
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-07-03 15:28:02 -0300

    Requisitando lista de provider

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -22,22 +22,36 @@ import {
   ProviderMetaText,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('/providers').then(response => {
+      setProviders(response.data);
+    });
+  });
 
   return (
     <Container>
       <Header>
         <HeaderTitle>
           Bem-vindo, {'\n'}
-          <UserName>Matheus Guermandi</UserName>
+          <UserName>{user.name}</UserName>
         </HeaderTitle>
 
         <ProfileButton>
           <UserAvatar
             source={{
-              uri:
-                'https://avatars2.githubusercontent.com/u/27836893?s=460&u=4d9a53c02a3d30c2e61c992f9ba603547129bfe0&v=4',
+              uri: user.avatar_url,
             }}
           />
         </ProfileButton>
