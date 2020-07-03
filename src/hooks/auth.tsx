2d83b9e76a7382ff5cfedfc0392a9ff6c25a384a
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-07-03 15:10:06 -0300

    Construindo header da página dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -10,9 +10,16 @@ import asyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -21,7 +28,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
